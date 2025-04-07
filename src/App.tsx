c3fa90d8897ee2f177df54d77a6b0b3d37508eab
Mohammed Fadl <mohammadefathl@gmail.com>
2025-04-08 00:54:32 +0300

    chore: add react router lib , as a data mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import reactRouterLogo from './assets/react-router.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://reactrouter.com" target="_blank">
+          <img src={reactRouterLogo} className="logo react-router" alt="React Router logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
